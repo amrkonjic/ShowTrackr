20d90cb1122175311374dd5ad3817ebe2c936eca
amrkonjic <mrkonjicanela@gmail.com>
2025-05-15 16:25:52 +0200

    create page for specific actor

---
+++ app/shows/[id]/cast/[castId]/page.js
@@ -0,0 +1,37 @@
+import Image from "next/image";
+
+export default async function Actor( {params} ){
+    const {castId} = await params;
+    const res = await fetch(`https://api.tvmaze.com/people/${castId}?embed=castcredits`);
+    const data = await res.json();
+
+
+    return(
+        <main className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 items-start">
+            <Image className="m-10"
+                src={data.image?.medium || "/actor.png"} 
+                alt={`${data.name} image`}
+                width={300}
+                height={100}
+                priority={true}
+                />
+
+            <div className="m-10 gap-8 flex flex-col content-between bg-indigo-950 p-4 border-solid border-white border-2 rounded-md shadow-md shadow-indigo-500/50">
+                <h2 className="font-bold">{data.name}</h2>
+                <p>Gender: {data.gender}</p>
+                <p>Country: {data.country?.name}</p>
+                <p>Birthday: {new Date(data.birthday).toLocaleDateString("hr-HR")}</p>
+                {data.deathday && <p>Deathday: {new Date(data.deathday).toLocaleDateString("hr-HR")}</p>}
+                
+            </div>
+            <div className="flex flex-col items-center gap-6 mt-8 bg-indigo-950 p-4 border-solid border-white border-2 rounded-md shadow-md shadow-indigo-500/50">
+                Shows:{data._embedded.castcredits.map(castcredit => (
+                    <div key={castcredit._links.show.name} className="flex flex-col gap-2 w-full items-center bg-violet-950 p-4 rounded-md border-b shadow-md shadow-indigo-100/50 ">
+                        <p>Character: {castcredit._links.character.name}</p>
+                        <p>Show: {castcredit._links.show.name}</p>
+                    </div>
+                ))}</div>
+        </main>
+    )
+}
+
--- app/shows/[id]/cast/page.js
+++ app/shows/[id]/cast/page.js
@@ -1,35 +1,34 @@
 import Image from "next/image";
+import Link from "next/link";
 
 
 export default async function Cast( {params} ) {
     const {id} = await params;
 
+    // podaci o glumcima koji su glumili u određenoj seriji
     const resCast = await fetch(`https://api.tvmaze.com/shows/${id}/cast`);
     const dataCast = await resCast.json();
-
+    // podaci o seriji koji sluze za prikaz slike u hero sekciji
     const res = await fetch(`https://api.tvmaze.com/shows/${id}`);
     const data = await res.json();
 
     return(
 
-        <div className="flex flex-col min-h-screen">
+        <div className="flex flex-col min-h-screen w-full items-center gap-6">
+            <h2 className="font-bold text-xl">Cast</h2>
             <div className="w-full h-[400px] relative mx-auto">
                 <Image
                     src={data.image?.original}
                     alt={`${data.name} banner`}
                     fill
-                    className="object-cover brightness-50"
+                    className="object-cover brightness-50 "
                     priority
                 />
                 <div className="absolute inset-0 bg-black/50" />
             </div>
                  
-
-            <div className="flex flex-col items-start gap-4 px-10">
             
-            <h2 className="font-bold text-xl">Cast</h2>
-            <div className="flex flex-col w-full gap-4">
-                <h2>Characters:</h2>
+            <div className="grid  sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-14">
                 {dataCast.map(actor => (
                     <div key={actor.character.id} className="flex items-center gap-4 w-full border-b pb-3">
                         <Image 
@@ -39,15 +38,16 @@ export default async function Cast( {params} ) {
                             height={100}
                             className="rounded-full object-cover w-[100px] h-[100px]"
                         />
-                        <p className="font-bold">{actor.character.name}</p>
-                        <p>{actor.person.name}</p>
+                        <div className="flex flex-col justify-center">
+                            <p className="font-bold">{actor.character.name}</p>
+                            <Link href={`/shows/${id}/cast/${actor.person.id}`}>{actor.person.name}</Link>
+                        </div>
+                        
                     </div>
                 ))}
             </div>
                 
             </div>
-        </div>
 
-        
     )
 }
